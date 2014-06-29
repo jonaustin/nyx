4378e9f7776a8a0556a3ad0e397cce5758434600
Jon Austin <jon.i.austin@gmail.com>
2014-06-29 14:54:59 -0700

    taste playlist created from last weeks top lastfm tracks

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -31,11 +31,11 @@ class PagesController < ApplicationController
     taste_json = playlist.taste_data_from_tracks_hash(tracks_hash).to_json
 
     taste_profile = Echowrap.taste_profile_create(:name => 'Weekly Lastfm Top Tracks', :type => 'song')
-    binding.pry 
     taste_id = taste_profile.id
     Rails.logger.info('TASTE ID: ' + taste_id)
     Echowrap.taste_profile_update(id: taste_id, data: taste_json)
-    en_songs = Echowrap.playlist_basic(seed_catalog: taste_id, type: 'catalog-radio', results: 50)
+    binding.pry 
+    en_songs = Echowrap.playlist_static(seed_catalog: taste_id, type: 'catalog-radio', results: 50)
     tracks_hash = playlist.echonest_tracks_to_hash(en_songs)
     @spotify_uris = playlist.spotify_playlist_from_tracks(tracks_hash)
     render :playlist
