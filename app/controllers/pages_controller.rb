b175439a0aae9fde1d0c1be80f0c1bdf5980a516
Jon Austin <jon.i.austin@gmail.com>
2014-06-29 11:11:18 -0700

    by user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,11 @@ class PagesController < ApplicationController
     @spotify_uris = Playlist.new.spotify_playlist_from_tracks
     render :home
   end
+
+  def top_tracks
+    user = params[:user] || 'echowarpt'
+    lastfm_tracks = Lastfm.new.top_weekly_tracks(user)
+    @spotify_uris = Playlist.new.spotify_playlist_from_tracks
+    render :home
+  end
 end
